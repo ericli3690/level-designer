baa7caac836e6a9681335aeb287e6fd8027d96a2
ericli3690 <ericli3690@gmail.com>
2021-05-20 12:00:37 -0600

    new blocks, right click works, hover for name, save properly

--- designer.js
+++ designer.js
@@ -5,7 +5,9 @@ var maxX = 0;
 var maxY = 0;
 var mouseIsDown = false;
 var usedButton = 0;
+var toOutput = 0;
 var output = [];
+var currentBrush = '';
 var ctx = document.getElementById('canvas').getContext('2d');
 
 //creates each block; this allows them to each have their own event listener
@@ -19,16 +21,16 @@ class Block {
       //grab the two classes, and split them into an array, with the first item being the colour
       classes = this.className.split(" ");
       //set it as the brush!
-      ctx.fillStyle = classes[0];
+      currentBrush = classes[0];
     })
   }
 }
 
 //the blocks
-var one, two, three, four, five;
-var blocks = [one, two, three, four, five];
-var names = ['dirt', 'grass', 'background', 'backgroundGrass', 'water'];
-var colours = ['purple', 'green', 'pink', 'red', 'blue'];
+var one, two, three, four, five, six, seven, eight, nine, ten;
+var blocks = [one, two, three, four, five, six, seven, eight, nine, ten];
+var names = ['spawn', 'win', 'overworld_material_dirt', 'overworld_material_grass', 'overworld_background', 'overworld_background_hanging_grass', 'overworld_background_hanging_grass_180', 'overworld_material_dirt_corner', 'overworld_material_dirt_corner_180', 'overworld_water'];
+var colours = ['yellow', 'black', 'purple', 'green', 'pink', 'red', 'orange', 'cyan', 'blue', 'navy'];
 
 //creates the blocks
 function createBlocks() {
@@ -48,6 +50,10 @@ function createBlocks() {
     var classs = document.createAttribute('class');
     classs.value = colours[i] + ' block';
     newButton.setAttributeNode(classs);
+
+    var title = document.createAttribute('title');
+    title.value = names[i];
+    newButton.setAttributeNode(title);
 
     //fill the canvas
     var ctxOfBlock = document.getElementById(names[i]).getContext('2d');
@@ -100,7 +106,12 @@ function createCanvas() {
         ctx.strokeStyle = 'black';
         ctx.strokeRect(j * 20, i * 20, 20, 20);
         if (output[i * maxX + j + 2] != 0) {
-          ctx.fillStyle = 'red';
+          ctx.fillStyle = colours[parseInt(output[i * maxX + j + 2], 10) - 1];
+          //i * maxx + j + 2 is the current tile of the string we are looking at
+          //output[the above] is that tile
+          //parseInt(the above, 10) takes the string output and turns it into a number, the 10 is for base 10
+          // the above - 1 because the numbers are off
+          //colours[the above]  to get the block with that call number
           ctx.fillRect(j * 20 + 1, i * 20 + 1, 18, 18);
         }
       }
@@ -124,12 +135,12 @@ function objectMousedOver() {
     var mouseY = event.clientY - 265 + pageYOffset;
     var targetX = Math.floor(mouseX / 20);
     var targetY = Math.floor(mouseY / 20);
-    var toOutput = 0;
-    if (usedButton == 0) {
-      toOutput = 1;
-    } else {
+    if (usedButton == 2) {
       ctx.fillStyle = 'white';
       toOutput = 0;
+    } else {
+      ctx.fillStyle = currentBrush;
+      toOutput = colours.indexOf(currentBrush) + 1;
     }
     ctx.fillRect(targetX * 20 + 1, targetY * 20 + 1, 18, 18);
     output[targetY * maxX + targetX + 2] = toOutput;
